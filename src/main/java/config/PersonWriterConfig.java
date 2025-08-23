1317d226a99e3602e418d22be7c41dd1ee13b40b
Jérémy Sabadie <sabadie.jeremy@gmail.comgit config --global user.name Jeremy>
2025-08-23 20:41:12 +0200

    Account éléments added.

--- src/main/java/config/PersonWriterConfig.java
+++ src/main/java/config/PersonWriterConfig.java
@@ -1,22 +1,28 @@
 package config;
 
+import javax.sql.DataSource;
 import model.Person;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
-
 @Configuration
 public class PersonWriterConfig {
 
     @Bean
-    public JdbcBatchItemWriter<Person> personJdbcWriter(DataSource dataSource) {
+    public JdbcBatchItemWriter<Person> personWriter(DataSource ds) {
         return new JdbcBatchItemWriterBuilder<Person>()
-                .dataSource(dataSource)
-                .sql("INSERT INTO person (first_name, last_name, email, age) VALUES (:firstName, :lastName, :email, :age)")
-                .beanMapped() 
-                .build();
+            .dataSource(ds)
+            .sql("""
+                INSERT INTO personne (first_name, last_name, email, age)
+                VALUES (:firstName, :lastName, :email, :age)
+                ON DUPLICATE KEY UPDATE
+                  first_name = VALUES(first_name),
+                  last_name  = VALUES(last_name),
+                  age        = VALUES(age)
+            """)
+            .beanMapped()
+            .build();
     }
 }
--- src/main/java/config/RunJobOnStartup.java
+++ src/main/java/config/RunJobOnStartup.java
@@ -3,29 +3,35 @@ package config;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.configuration.JobRegistry;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
 
 @Component
-@Profile("!test") // pas pendant les tests
+@Profile("!test")
 public class RunJobOnStartup implements ApplicationRunner {
 
     private final JobLauncher jobLauncher;
-    private final Job importPersonJob;
+    private final JobRegistry jobRegistry;
 
-    public RunJobOnStartup(JobLauncher jobLauncher, Job importPersonJob) {
+    @Value("${spring.batch.job.name:importPersonJob}")
+    private String jobName; // default = importPersonJob
+
+    public RunJobOnStartup(JobLauncher jobLauncher, JobRegistry jobRegistry) {
         this.jobLauncher = jobLauncher;
-        this.importPersonJob = importPersonJob;
+        this.jobRegistry = jobRegistry;
     }
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        Job job = jobRegistry.getJob(jobName);
         JobParameters params = new JobParametersBuilder()
-                .addLong("ts", System.currentTimeMillis())
+                .addLong("ts", System.currentTimeMillis(), true)
                 .toJobParameters();
-        jobLauncher.run(importPersonJob, params);
+        jobLauncher.run(job, params);
     }
 }
